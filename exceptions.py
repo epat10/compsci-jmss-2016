b9f0bc7f3bb588e7c7c793bbfaa8409ce9d166cf
Ellie <pat0018@jmss.vic.edu.au>
2016-02-08 14:58:53 +1100

    Done some looping to Exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,19 @@
 my_list = [1,2,3,5]
 
-#try:
-x = my_list[5]
-#except IndexError:
-#    print("Out of Range")
+try:
+    x = my_list[5]
+except IndexError:
+    print("Out of Range")
+
+    
+notDone = True
+while notDone:
+    response = input("Do you like people?")
+    if response.lower() == "yes":
+        print("I like people too.")
+        notDone = False
+    elif response.lower() == "no":
+        print("Oh, ok.")
+        notDone = False
+    else:
+        print("Enter yes or no please!")
