4c127e56d6657a2e034634055f8e58cb15d5ba8e
Ellie <pat0018@jmss.vic.edu.au>
2016-02-29 15:08:04 +1100

    Test finished... sort of

--- tests/t1/words.py
+++ tests/t1/words.py
@@ -1,2 +1,19 @@
 # Write a program to read in words from the keyboard one at a time until the word "quit" is typed.
 # Store them in a list then print them alphabetically
+
+list_words = []
+
+
+quit = False
+while False:
+    response = input("Enter a word please")
+    list_words.append(response)
+    if response.strip() == "quit":
+        quit == True
+
+list_words = sort.list_words()
+print(list_words)
+
+
+
+
