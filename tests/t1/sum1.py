55428b74503d2ee3343086f60318d2e3c55aacf7
Ellie <pat0018@jmss.vic.edu.au>
2016-03-01 11:27:24 +1100

    Done some looping to Exceptions.py

--- tests/t1/sum1.py
+++ tests/t1/sum1.py
@@ -1,1 +1,10 @@
 # write a program that reads in 10 numbers, then prints the sum of those
+
+
+n = []
+for range in (0, 11, 1):
+    val = int("Please enter a number")
+    val + n
+
+
+print(n)
--- tests/t1/sum2.py
+++ tests/t1/sum2.py
@@ -1,2 +1,15 @@
 # copy the code from sum1.py into this file, THEN:
 # change your program so it keeps reading numbers until it gets a -1, then prints the sum of all numbers read
+
+val_sum = []
+
+x = input("Please enter a number.")
+
+
+
+while False:
+    val_sum.append(x)
+    if True:
+        print(sum(val_sum))
+
+
